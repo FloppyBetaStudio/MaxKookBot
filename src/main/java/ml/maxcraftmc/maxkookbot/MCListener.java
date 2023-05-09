5457b73e9ea4ca53c20d10a159fbca8313c1e71f
软盘君 <2459681512@qq.com>
2023-05-09 20:05:40 +0800

    将Listener作为一个独立的Class

---
+++ src/main/java/ml/maxcraftmc/maxkookbot/MCListener.java
@@ -0,0 +1,64 @@
+package ml.maxcraftmc.maxkookbot;
+
+import org.bukkit.advancement.Advancement;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import static org.bukkit.event.EventPriority.MONITOR;
+
+public class MCListener implements Listener{
+    @EventHandler(priority = MONITOR)
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+        String s = "<" + event.getPlayer().getName() + ">" + event.getMessage();
+        KookMain.sendMessageAsync(s);
+
+
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        // 玩家加入游戏
+        Player player = event.getPlayer();
+        String message = player.getName() + " 加入了游戏!";
+        KookMain.sendMessageAsync(message);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        // 玩家退出游戏
+        Player player = event.getPlayer();
+        String message = player.getName() + " 退出了游戏!";
+        KookMain.sendMessageAsync(message);
+    }
+
+    // 玩家死亡
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        String deathMessage = player.getName() + " 死了!";
+        if (event.getDeathMessage() != null) {
+            deathMessage += " 死因:" + event.getDeathMessage();
+        }
+        KookMain.sendMessageAsync(deathMessage);
+    }
+
+    // 玩家获得成就
+    @EventHandler
+    public void onPlayerAdvancementDone(PlayerAdvancementDoneEvent event) {
+        Player player = event.getPlayer();
+        Advancement achievement = event.getAdvancement();
+        String message = player.getName() + " 获得了成就 " + achievement.getDisplay();
+        KookMain.sendMessageAsync(message);
+    }
+}
+
+
--- src/main/java/ml/maxcraftmc/maxkookbot/MaxKookBot.java
+++ src/main/java/ml/maxcraftmc/maxkookbot/MaxKookBot.java
@@ -44,7 +44,7 @@ public final class MaxKookBot extends JavaPlugin {
                 throw new RuntimeException(e);
             }
         }
-        getServer().getPluginManager().registerEvents(new BotListener(), this);
+        getServer().getPluginManager().registerEvents(new MCListener(), this);
 
     }
 
@@ -54,54 +54,6 @@ public final class MaxKookBot extends JavaPlugin {
         KookMain.SendMessageToChannel("机器人关机");
     }
 
-    public class BotListener implements Listener {
-        @EventHandler(priority = MONITOR)
-        public void onPlayerChat(AsyncPlayerChatEvent event) {
-            if (event.isCancelled() ){
-                return;
-            }
-            String s = "<" + event.getPlayer().getName() + ">" + event.getMessage();
-            KookMain.sendMessageAsync(s);
-
-
-        }
-
-        @EventHandler
-        public void onPlayerJoin(PlayerJoinEvent event) {
-            // 玩家加入游戏
-            Player player = event.getPlayer();
-            String message = player.getName() + " 加入了游戏!";
-            KookMain.sendMessageAsync(message);
-        }
-
-        @EventHandler
-        public void onPlayerQuit(PlayerQuitEvent event) {
-            // 玩家退出游戏
-            Player player = event.getPlayer();
-            String message = player.getName() + " 退出了游戏!";
-            KookMain.sendMessageAsync(message);
-        }
-
-        // 玩家死亡
-        @EventHandler
-        public void onPlayerDeath(PlayerDeathEvent event) {
-            Player player = event.getEntity();
-            String deathMessage = player.getName() + " 死了!";
-            if (event.getDeathMessage() != null) {
-                deathMessage += " 死因:" + event.getDeathMessage();
-            }
-            KookMain.sendMessageAsync(deathMessage);
-        }
-
-        // 玩家获得成就
-        @EventHandler
-        public void onPlayerAdvancementDone(PlayerAdvancementDoneEvent event) {
-            Player player = event.getPlayer();
-            Advancement achievement = event.getAdvancement();
-            String message = player.getName() + " 获得了成就 " + achievement.getDisplay();
-            KookMain.sendMessageAsync(message);
-        }
-    }
 
 
 }
